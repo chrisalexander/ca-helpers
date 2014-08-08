bed68d842f6a4d9c9b663756e48473d6bff02c7a
Chris Alexander <chris@chris-alexander.co.uk>
2014-08-08 17:29:58 +0100

    Resolve to one file, and add "once"

--- ca-helpers.js
+++ ca-helpers.js
@@ -1,1 +1,52 @@
 angular.module("ca-helpers", []);
+
+angular.module("ca-helpers")
+
+.factory("caHelpersApply", function($rootScope, $log) {
+
+  var apply = function(scope) {
+    if (!scope) { return; }
+    switch (scope.$$phase) {
+      case "$digest":
+      case "$apply":
+        $log.info("Apply not applied, current phase: ", scope.$$phase);
+        break;
+      default:
+        try {
+          scope.$apply();
+        } catch (e) {
+          $log.info("Attempted to apply, but received exception: ", e)
+        }
+    }
+  }
+
+  return function() {
+    $rootScope.apply = function() {
+      return apply($rootScope);
+    }
+  };
+
+})
+
+.run(function(caHelpersApply) {
+  caHelpersApply();
+});
+
+angular.module("ca-helpers")
+
+.factory("caHelpersOnce", function($rootScope) {
+
+  var once = function(scope, event, fn) {
+    var removeFunction;
+    removeFunction = scope.$on(event, function() {
+      try {
+        fn();
+      } finally {
+        removeFunction();
+      }
+    });
+  }
+
+  return once;
+
+});
